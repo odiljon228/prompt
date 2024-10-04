b5b70832a07a5315bc2885802fa3aa7b0cca863d
jamshid-dev <jamshidt234@gmail.com>
2024-10-04 09:49:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let userName = prompt('UserName kiriting')
+let login = prompt('Login kirit') // string
+let parol = prompt('Parol kirit') // string
+
+
+
+if(login == "test" && parol == "test"){
+    alert(`${userName} , saytga hush kelibsiz`)
+}else{
+    alert('Qaydadan urinib ko\'ring')
+}
